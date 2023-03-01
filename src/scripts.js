d24b0329d7e72032584114b4aa222b2dbe88df6f
Kara Anderson <kanderson8585@gmail.com>
2023-02-28 20:13:49 -0700

    Build out HTML sketelon and CSS styling

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,4 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-
-
-console.log('This is the JavaScript entry file - your code begins here.');
+import './images/lobby.jpg'
